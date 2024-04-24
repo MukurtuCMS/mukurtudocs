f571210ebf18a7340ad5c3ba262adc7048e8a4f5
Michael Wynne <88502274+michael-wynne-wsu@users.noreply.github.com>
2024-04-24 15:41:12 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['source/_static']
 html_css_files = [
     'css/mukurtu.css'
 ]
