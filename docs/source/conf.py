998eceb618ed7d5e5c7e0c32d7bfe6121c61383e
Michael Wynne <88502274+michael-wynne-wsu@users.noreply.github.com>
2024-05-15 11:24:22 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Mukurtu Support'
+project = 'Mukurtu 4 User Manual'
 copyright = '2024, Mukurtu'
 author = 'Mukurtu'
 release = '0.1'
